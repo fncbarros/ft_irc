ffd94e48b18f31cd666f4ed2a3c0fe2c268158e0
fbarros <fbarros@student.42lisboa.com>
2023-07-07 17:58:51 +0100

    renamed binary

--- source/ircserv.cpp
+++ source/ircserv.cpp
@@ -1,12 +1,17 @@
+#include <string>
+
 // TODO: dummy compilable source file
 int main(int argc, char* argv[])
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
 		return 1;
 	}
 
-	(void)argv;
+	int port = std::atoi(argv[1]);
+	
+	std::string password(argv[2]);
+
 
 	return 0;
 }
